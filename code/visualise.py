a0a005c2d5d26513b77cbbbcb80150ab72da141f
DavidMiles-Skov <milesskovd@gmail.com>
2024-08-25 13:25:18 +0200

    Finished initial visualiser file. Added solver.py

--- code/visualise.py
+++ code/visualise.py
@@ -35,22 +35,69 @@ def plot_supply_demand(df_supply, df_demand, save=False):
     if save:
         save_fig(fig)
 
-    # Show the plot
-    plt.show()
+    return fig,ax
 
 def plot_social_welfare():
     return
 
-def plot_AvsO_help(df,type ,save):
+def gen_accepted_vs_offered(df,type,result,N_G,save):
+    
+    # Set up
+    
+    labels = df['ID'] 
+    
     if type=='Supply':
         colours = ['lightblue', 'midnightblue']
+        actual =  result.x[:N_G]
+        offer = df[df['Supply/Demand']=='Supply']['Quantity'].to_numpy()
     else:
-        colours = ['lightcoral', 'darkred']
+        colors = ['lightcoral', 'darkred']
+        actual = result.x[N_G::]
+        offer = df[df['Supply/Demand']=='Demand']['Quantity'].to_numpy()
+
+
+    y_pos = np.arange(len(labels))
+
+    # Plotting 
+
+    fig, ax = plt.subplots()
+    # make figure wider
+    fig.set_figwidth(14)
+    ax.barh(y_pos, offer, alpha=0.6, color=colors[0],label='Offered')
+
+    # Going over, IDs again, indicating actual amount of electricity supplied
+
+    ax.barh(y_pos, actual, alpha=0.8, color=colors[1], label='Accepted')
+
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(labels, fontsize=10)
+    ax.set_xlabel('MWh')
+    ax.set_ylabel('ID')
+    ax.set_title("Supply (Accepted + Offered)")
+    plt.xticks(rotation=45)
+    plt.legend(loc='upper right')
+
+    if save:
+        save_fig(fig)
+
+    return fig, ax
+        
     
 
 
 
-def plot_accepted_vs_offered(df_supply, df_demand, save=False):
+def plot_accepted_vs_offered(df_supply, df_demand, N_G, N_D,save=False):
+
+    # For supply
+
+    fig_supply, ax_supply = gen_accepted_vs_offered_help(df_supply, 'Supply', N_G, save)
+    plt.show()
+
+    # For demand
+    fig_demand, ax_demand = gen_accepted_vs_offered(df_demand, 'Demand', N_G, save)
+    plt.show()
+
+
 
 
 
